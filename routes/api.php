fc63dd51a1a62914cca942c33fdac4775b014a41
J-T-McC <tyson_mcc@hotmail.com>
2020-12-14 22:45:53 -0800

    Api route & permission updates
    - user must be verified to access resource endpoints
    - authenticated route now returns basic information

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
+
     Route::get('/authenticated', function () {
-        return ['message' => 'authenticated'];
+        return new \App\Http\Resources\UserResource(auth()->user());
+    })->middleware();
+
+    Route::middleware(['verified'])->group(function () {
+        Route::resource('/monitors', \App\Http\Controllers\MonitorController::class);
+        Route::resource('/channels', \App\Http\Controllers\ChannelController::class);
+        Route::resource('/monitors-channels', \App\Http\Controllers\MonitorChannelController::class, ['PUT', 'PATCH']);
     });
-    Route::resource('/monitors', \App\Http\Controllers\MonitorController::class);
-    Route::resource('/channels', \App\Http\Controllers\ChannelController::class);
-    Route::resource('/monitors-channels', \App\Http\Controllers\MonitorChannelController::class, ['PUT', 'PATCH']);
 });
