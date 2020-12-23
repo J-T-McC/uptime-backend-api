a1a93d7c3d6b10d815763dfd4e7f33484e078dd0
J-T-McC <tyson_mcc@hotmail.com>
2020-12-22 16:47:04 -0800

    Removed token auth related classes

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable;
 
     public function getKey(): string
     {
