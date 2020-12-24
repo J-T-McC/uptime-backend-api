b9a91924e6e10937fb5aec6ccd74957e6a2855db
J-T-McC <tyson_mcc@hotmail.com>
2020-12-23 22:34:57 -0800

    Moved user is owner global scope into trait

---
+++ app/Models/Traits/UsesOwnerScope.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Models\Traits;
+
+use App\Models\Scopes\OwnerScope;
+
+trait UsesOwnerScope
+{
+
+    protected static function bootUsesOwnerScope()
+    {
+        if(!app()->runningInConsole()) {
+            static::addGlobalScope(new OwnerScope);
+        }
+    }
+
+}
