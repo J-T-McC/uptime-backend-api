dcbc43d4b8a42f5c9d316c51382d13111ba56be8
J-T-McC <tyson_mcc@hotmail.com>
2021-01-19 19:45:06 -0800

    Adding atomic deployments package

---
+++ config/atomic-deployments.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /**
+     * Symbolic link to the current deployed build
+     * This path should be used for schedules and setting your web root
+     */
+    'deployment-link' => env('ATM_DEPLOYMENT_LINK'),
+
+    /**
+     * The primary build folder
+     * This folder is where all deployments ran and ultimately copied to a deployment directory
+     */
+    'build-path' => env('ATM_BUILD'),
+
+    /**
+     * Production build directory
+     * Builds are copied here and linked for deployment
+     * Ensure this directory has the required permissions to allow php and your webserver to run your application here
+     */
+    'deployments-path' => env('ATM_DEPLOYMENTS'),
+
+    /**
+     * Max number of build directories allowed
+     * Once limit is hit, old deployments will be removed automatically after a successful build
+     */
+    'build-limit' => 5
+];
